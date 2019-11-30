ed22fb62538b160ad7de4a3170eb23c6cca1287b
svagenas <spyros115@gmail.com>
2019-12-01 01:03:01 +0200

    History class

--- app/src/main/java/com/svag/exp/MainActivity.java
+++ app/src/main/java/com/svag/exp/MainActivity.java
@@ -2,13 +2,21 @@ package com.svag.exp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn = (Button)findViewById(R.id.button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
 
 
 
